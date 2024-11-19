dfaccba1976a3e7bf4cf6a977e0318d0be82d880
Thentrixxx <133041011+Thentrixxx@users.noreply.github.com>
2024-11-19 01:22:16 -0700

    Add files via upload

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -6,11 +6,9 @@ var shortcuts = {
 	"Shortcut_Placeholder": {"Score": 0, "Priority": 0}
 }
 
-
 # Function on startup for testing.
 func _ready():
-	pass
-
+	give_level()
 
 # Function that adds a shortcut to the dictionary "shortcuts"
 func add_shortcut(shortcut: String, score: int, priority: int):
@@ -54,3 +52,107 @@ func print_shortcuts():
 			print(str(x) + ": " + str(shortcuts[i][x]))
 		print()
 	print("----------------------")
+	
+
+# Function responsible for giving the player a level based on the dictionary.
+func give_level():
+	var next_level_selected
+	var next_level_level = 26
+	for i in shortcuts:
+		var is_priority = false
+		var shortcut = i
+		var score: int
+		var priority: int
+		
+		for x in shortcuts[i]:
+			if is_priority == false:
+				score = shortcuts[i][x]
+				is_priority = true
+			elif is_priority == true:
+				priority = shortcuts[i][x]
+				is_priority = false
+		print(str(score))
+		print(str(priority))
+		
+		var level = 999
+		
+		if priority == 0:
+			level = 999
+		
+		elif priority == 1:
+			if score <= 99:
+				level = 1
+			elif score <= 199:
+				level = 2
+			elif score <= 299:
+				level = 4
+			elif score <= 399:
+				level = 7
+			elif score <= 499:
+				level = 11
+			else:
+				pass
+			
+		elif priority == 2:
+			if score <= 99:
+				level = 3
+			elif score <= 199:
+				level = 5
+			elif score <= 299:
+				level = 8
+			elif score <= 399:
+				level = 12
+			elif score <= 499:
+				level = 16
+			else:
+				pass
+			
+		elif priority == 3:
+			if score <= 99:
+				level = 6
+			elif score <= 199:
+				level = 9
+			elif score <= 299:
+				level = 13
+			elif score <= 399:
+				level = 17
+			elif score <= 499:
+				level = 20
+			else:
+				pass
+			
+		elif priority == 4:
+			if score <= 99:
+				level = 10
+			elif score <= 199:
+				level = 14
+			elif score <= 299:
+				level = 18
+			elif score <= 399:
+				level = 21
+			elif score <= 499:
+				level = 23
+			else:
+				pass
+			
+		elif priority == 5:
+			if score <= 99:
+				level = 15
+			elif score <= 199:
+				level = 19
+			elif score <= 299:
+				level = 22
+			elif score <= 399:
+				level = 24
+			elif score <= 499:
+				level = 25
+			else:
+				pass
+		
+		print(str(level))
+		# If this level's level is lower than the current one, select this one instead.
+		if next_level_level > level:
+			next_level_selected = shortcut
+			next_level_level = level
+	
+	print(str(next_level_selected))
